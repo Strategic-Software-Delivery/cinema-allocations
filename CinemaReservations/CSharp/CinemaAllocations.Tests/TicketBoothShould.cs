43fe7a8aa92b42ab6272554b7776d9ce5db5330f
Kenny Baas-Schwegler <kenny@baasie.com>
2020-08-31 11:33:20 +0200

    add test to reserve multiple seats

--- CinemaReservations/CSharp/CinemaAllocations.Tests/TicketBoothShould.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Tests/TicketBoothShould.cs
@@ -21,6 +21,23 @@ namespace CinemaAllocations.Tests
             Check.That(seatsAllocated.ReservedSeats).HasSize(1);
             Check.That(seatsAllocated.ReservedSeats[0].ToString()).IsEqualTo("A3");
         }
+        
+        [Test]
+        public void Reserve_multiple_seats_when_available()
+        {
+            const string showId = "3";
+            const int partyRequested = 3;
+
+            IMovieScreeningRepository repository = new StubMovieScreeningRepository();
+            TicketBooth ticketBooth = new TicketBooth(repository);
+
+            var seatsAllocated = ticketBooth.AllocateSeats(new AllocateSeats(showId, partyRequested));
+
+            Check.That(seatsAllocated.ReservedSeats).HasSize(3);
+            Check.That(seatsAllocated.ReservedSeats[0].ToString()).IsEqualTo("A6");
+            Check.That(seatsAllocated.ReservedSeats[1].ToString()).IsEqualTo("A7");
+            Check.That(seatsAllocated.ReservedSeats[2].ToString()).IsEqualTo("A8");
+        }
 
         [Fact]
         public void Return_SeatsNotAvailable_when_all_seats_are_unavailable()
