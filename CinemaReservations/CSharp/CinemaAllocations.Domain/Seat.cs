6878212d38e9b4a0ad98bab20dd854ada824b3ba
Kenny Baas-Schwegler <kenny@baasie.com>
2020-09-20 12:50:27 +0200

    Passing reserve_three_adjacent_seats_when_available

--- CinemaReservations/CSharp/CinemaAllocations.Domain/Seat.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Domain/Seat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Value;
 
 namespace CinemaAllocations.Domain
@@ -8,9 +9,9 @@ namespace CinemaAllocations.Domain
     {
         public string RowName { get; }
         public uint Number { get; }
-        
+
         public SeatAvailability SeatAvailability { get; }
-        
+
         public Seat(string rowName, uint number, SeatAvailability seatAvailability)
         {
             RowName = rowName;
@@ -32,16 +33,29 @@ namespace CinemaAllocations.Domain
         {
             return RowName.Equals(seat.RowName) && Number == seat.Number;
         }
-        
+
+        public bool IsAdjacentWith(List<Seat> seats)
+        {
+            var orderedSeats = seats.OrderBy(s => s.Number).ToList();
+
+            foreach (var seat in orderedSeats)
+            {
+                if (Number + 1 == seat.Number || Number - 1 == seat.Number)
+                    return true;
+            }
+
+            return false;
+        }
+
         public override string ToString()
         {
             return $"{RowName}{Number}";
         }
-        
-        
+
+
         protected override IEnumerable<object> GetAllAttributesToBeUsedForEquality()
         {
-            return new object[] { RowName, Number, SeatAvailability };
+            return new object[] {RowName, Number, SeatAvailability};
         }
     }
 }
