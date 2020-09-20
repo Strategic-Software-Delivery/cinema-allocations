a2be28fba42ca84fd2028ec9c7e3817c9a4056c3
Kenny Baas-Schwegler <kenny@baasie.com>
2020-09-20 16:09:28 +0200

    C# lab3begin

---
+++ CinemaReservations/CSharp/CinemaAllocations.Infra/Repository/MovieScreeningContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CinemaAllocations.Infra.Repository.model
+{
+    public class MovieScreeningContext: DbContext
+    {
+        public MovieScreeningContext() : base()
+        {
+            
+        }
+        
+        
+    }
+}
