ea5281cbac290393163838779617849c9840af64
João Rosa <joaoasrosa@gmail.com>
2020-10-09 15:30:51 +0200

    Add tests

--- CinemaReservations/CSharp/CinemaAllocations.Tests.Integration/Given.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Tests.Integration/Given.cs
@@ -13,41 +13,44 @@ namespace CinemaAllocations.Tests.Integration
         internal static class The
         {
             internal static string FordTheaterId => "1";
-            
-            internal static IMovieScreeningRepository FordTheater
+
+            internal static IMovieScreeningRepository FordTheater => RetrieveMovieScreeningFromJson(FordTheaterId);
+
+            internal static string DockStreetId => "3";
+            internal static IMovieScreeningRepository DockStreet => RetrieveMovieScreeningFromJson(DockStreetId);
+
+            private static IMovieScreeningRepository RetrieveMovieScreeningFromJson(string showId)
             {
-                get
+                var options = new DbContextOptionsBuilder<CinemaContext>()
+                    .UseInMemoryDatabase(databaseName: "CinemaTest")
+                    .Options;
+
+                var cinemaContext = new CinemaContext(options);
+
+                var directoryName = $"{GetExecutingAssemblyDirectoryFullPath()}\\MovieScreenings\\";
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ||
+                    RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
-                    var options = new DbContextOptionsBuilder<CinemaContext>()
-                        .UseInMemoryDatabase(databaseName: "CinemaTest")
-                        .Options;
-                    
-                    var cinemaContext = new CinemaContext(options);
-                    
-                    var directoryName = $"{GetExecutingAssemblyDirectoryFullPath()}\\MovieScreenings\\";
-
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                    {
-                        directoryName = $"{GetExecutingAssemblyDirectoryFullPath()}/MovieScreenings/";
-                    }
-
-                    foreach (var fileFullName in Directory.EnumerateFiles($"{directoryName}"))
-                    {
-                        var fileName = Path.GetFileName(fileFullName);
-                        var eventId = Path.GetFileName(fileName.Split("-")[0]);
-                        
-                        if (eventId != FordTheaterId) continue;
-                        
-                        var movieScreeningDto = JsonFile.ReadFromJsonFile<MovieScreeningDto>(fileFullName);
-
-                        cinemaContext.MovieScreenings.Add(movieScreeningDto.ToDataModel(eventId));
-                        cinemaContext.SaveChanges();
-                        
-                        break;
-                    }
-                    
-                    return new MovieScreeningRepository(cinemaContext);
+                    directoryName = $"{GetExecutingAssemblyDirectoryFullPath()}/MovieScreenings/";
                 }
+
+                foreach (var fileFullName in Directory.EnumerateFiles($"{directoryName}"))
+                {
+                    var fileName = Path.GetFileName(fileFullName);
+                    var eventId = Path.GetFileName(fileName.Split("-")[0]);
+
+                    if (eventId != showId) continue;
+
+                    var movieScreeningDto = JsonFile.ReadFromJsonFile<MovieScreeningDto>(fileFullName);
+
+                    cinemaContext.MovieScreenings.Add(movieScreeningDto.ToDataModel(eventId));
+                    cinemaContext.SaveChanges();
+
+                    break;
+                }
+
+                return new MovieScreeningRepository(cinemaContext);
             }
 
             private static string GetExecutingAssemblyDirectoryFullPath()
--- CinemaReservations/CSharp/CinemaAllocations.Tests.Integration/TicketBoothShould.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Tests.Integration/TicketBoothShould.cs
@@ -19,5 +19,19 @@ namespace CinemaAllocations.Tests.Integration
             Check.That(seatsAllocated.ReservedSeats).HasSize(1);
             Check.That(seatsAllocated.ReservedSeats[0].ToString()).IsEqualTo("A3");
         }
+        
+        [Fact]
+        public void Reserve_multiple_seats_when_available()
+        {
+            const int partyRequested = 3;
+
+            IMovieScreeningRepository repository = Given.The.DockStreet;
+            TicketBooth ticketBooth = new TicketBooth(repository);
+
+            var seatsAllocated = ticketBooth.AllocateSeats(new AllocateSeats(Given.The.DockStreetId, partyRequested));
+
+            Check.That(seatsAllocated.ReservedSeats).HasSize(3);
+            Check.That(seatsAllocated.SeatNames()).ContainsExactly("A6", "A7", "A8");
+        }
     }
 }
