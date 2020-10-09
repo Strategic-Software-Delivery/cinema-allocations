41b1f1efeb4b56b06b8a8cfd00d4fb84dad38be3
João Rosa <joaoasrosa@gmail.com>
2020-10-09 14:30:04 +0200

    Setup the basis of the lab 3 begin

---
+++ CinemaReservations/CSharp/CinemaAllocations.Infra.DataPersistence/MovieScreeninRepository.cs
@@ -0,0 +1,12 @@
+using CinemaAllocations.Domain;
+
+namespace CinemaAllocations.Infra
+{
+    public class MovieScreeninRepository : IMovieScreeningRepository
+    {
+        public MovieScreening FindMovieScreeningById(string screeningId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
