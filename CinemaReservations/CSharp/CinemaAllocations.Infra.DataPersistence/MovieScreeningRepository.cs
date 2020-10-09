0d228ff28271ae86b50f83ca6b1572dfe78ef49e
João Rosa <joaoasrosa@gmail.com>
2020-10-09 14:43:11 +0200

    Rename

--- CinemaReservations/CSharp/CinemaAllocations.Infra.DataPersistence/MovieScreeningRepository.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Infra.DataPersistence/MovieScreeningRepository.cs
@@ -2,7 +2,7 @@ using CinemaAllocations.Domain;
 
 namespace CinemaAllocations.Infra.DataPersistence
 {
-    public class MovieScreeninRepository : IMovieScreeningRepository
+    public class MovieScreeningRepository : IMovieScreeningRepository
     {
         public MovieScreening FindMovieScreeningById(string screeningId)
         {
