8fefd4a4c4e28f0ab5ea6c174aabfeb7d5f47c59
João Rosa <joaoasrosa@gmail.com>
2020-10-12 16:00:44 +0200

    Fix test

--- CinemaReservations/CSharp/CinemaAllocations.Tests/TicketBoothShould.cs
+++ CinemaReservations/CSharp/CinemaAllocations.Tests/TicketBoothShould.cs
@@ -22,7 +22,7 @@ namespace CinemaAllocations.Tests
             Check.That(seatsAllocated.ReservedSeats[0].ToString()).IsEqualTo("A3");
         }
         
-        [Test]
+        [Fact]
         public void Reserve_multiple_seats_when_available()
         {
             const string showId = "3";
@@ -66,8 +66,8 @@ namespace CinemaAllocations.Tests
 
         }
         
-        [Test]
-        public void reserve_three_adjacent_seats_when_available()
+        [Fact]
+        public void Reserve_three_adjacent_seats_when_available()
         {
             const string showId = "2";
             const int partyRequested = 3;
@@ -82,8 +82,8 @@ namespace CinemaAllocations.Tests
 
         }
         
-        [Test]
-        public void return_NoPossibleAdjacentSeatsFound_when_4_tickets_are_requested()
+        [Fact]
+        public void Return_NoPossibleAdjacentSeatsFound_when_4_tickets_are_requested()
         {
             const string showId = "2";
             const int partyRequested = 4;
