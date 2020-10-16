25ef11a63984496abf367ce3a1e2a18b0f468cf8
João Rosa <joaoasrosa@gmail.com>
2020-10-16 15:23:15 +0200

    Add clue

---
+++ CinemaReservations/CSharp/CinemaAllocations.Infra.DataPersistence/MovieScreening.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CinemaAllocations.Infra
+{
+    public class MovieScreening
+    {
+        internal Domain.MovieScreening ToDomainModel()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
