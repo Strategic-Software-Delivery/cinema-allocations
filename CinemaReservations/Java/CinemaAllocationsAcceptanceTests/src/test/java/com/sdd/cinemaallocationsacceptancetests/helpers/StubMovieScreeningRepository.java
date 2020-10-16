677dd2850bcd9d3aa341dde0e40622566aaea385
Kenny Baas-Schwegler <kenny@baasie.com>
2020-10-16 11:50:10 +0200

    Refactored stubbing and testing

--- CinemaReservations/Java/CinemaAllocationsAcceptanceTests/src/test/java/com/sdd/cinemaallocationsacceptancetests/helpers/StubMovieScreeningRepository.java
+++ CinemaReservations/Java/CinemaAllocationsAcceptanceTests/src/test/java/com/sdd/cinemaallocationsacceptancetests/helpers/StubMovieScreeningRepository.java
@@ -1,9 +1,11 @@
-package com.sdd.cinemaallocationsacceptancetests.StubMovieScreening;
+package com.sdd.cinemaallocationsacceptancetests.helpers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.google.common.collect.ImmutableList;
 import com.sdd.cinemaallocations.*;
+import com.sdd.cinemaallocationsacceptancetests.helpers.stubmodel.MovieScreeningDto;
+import com.sdd.cinemaallocationsacceptancetests.helpers.stubmodel.SeatDto;
 
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
@@ -33,50 +35,12 @@ public class StubMovieScreeningRepository implements MovieScreeningRepository {
 
     @Override
     public MovieScreening findMovieScreeningById(final String showId)  {
-        MovieScreeningDto movieScreeningDto;
-
         if (repository.containsKey(showId)) {
-            movieScreeningDto = repository.get(showId);
+            return repository.get(showId).toDomain();
         } else {
             throw new IllegalArgumentException("MovieScreening not found for screening ID: " + showId);
         }
-
-        Map<String, Row> rows = new HashMap<>();
-
-        for (Map.Entry<String, ImmutableList<SeatDto>> rowDto : movieScreeningDto.rows().entrySet()) {
-            List<Seat> seats = new ArrayList<>();
-
-            rowDto.getValue().forEach(seatDto -> {
-                String rowName = rowDto.getKey();
-                int number = extractNumber(seatDto.name());
-
-                SeatAvailability seatAvailability = extractAvailability(seatDto.seatAvailability());
-
-
-                seats.add(new Seat(rowName, number, seatAvailability));
-            });
-
-            rows.put(rowDto.getKey(), new Row(rowDto.getKey(), seats));
-        }
-
-        return new MovieScreening(rows);
     }
 
-    private SeatAvailability extractAvailability(final String seatAvailability) {
 
-        switch(seatAvailability) {
-            case "Available":
-                return SeatAvailability.Available;
-            case "Reserved":
-                return SeatAvailability.Reserved;
-            case "Confirmed":
-                return SeatAvailability.Confirmed;
-            default:
-                throw new IllegalStateException("Unexpected value: " + seatAvailability);
-        }
-    }
-
-    private static int extractNumber(final String name) {
-        return Integer.parseUnsignedInt(name.substring(1));
-    }
 }
